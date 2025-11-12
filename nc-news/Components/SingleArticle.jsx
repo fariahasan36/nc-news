5f10ca932a055c59ef797af3d8647a444b183737
Faria Hasan Talukder <faria1136@gmail.com>
2025-11-12 15:25:19 +0000

    Add optimistic rendering on Voting an article

--- nc-news/Components/SingleArticle.jsx
+++ nc-news/Components/SingleArticle.jsx
@@ -1,19 +1,45 @@
 import { useState, useEffect } from "react";
-import { fetchArticleById } from "../api";
-import { useParams } from "react-router";
+import { fetchArticleById, PatchArticlesByArticleId } from "../api";
+import { Link, useParams } from "react-router";
 
 export default function SingleArticle() {
   const [article, setArticle] = useState({});
   const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   let params = useParams();
 
   useEffect(() => {
-    fetchArticleById(params.id).then((data) => {
-      setArticle(data.article);
-      setLoading(false);
-    });
+    fetchArticleById(params.id)
+      .then((data) => {
+        setArticle(data.article);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error);
+      });
   }, []);
+
+  const { article_id } = article;
+
+  function increaseVoteByArticleId() {
+    setArticle({ ...article, votes: article.votes + 1 });
+    PatchArticlesByArticleId(article_id, 1)
+      .then(() => {
+        return fetchArticleById(article_id);
+      })
+      .then((data) => {
+        setArticle(data.article);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setArticle(article);
+        setError(error);
+      });
+  }
+
   if (isLoading) return <p>Loading</p>;
   // const utcDate1 = new Date(article.created_at);
   return (
@@ -30,9 +56,14 @@ export default function SingleArticle() {
           <p>Author: {article.author}</p>
           <p>Topic: {article.topic}</p>
           <p>Body: {article.article_body}</p>
-          <p>Votes: {article.votes}</p>
           <p>Comment Count: {article.comment_count}</p>
           <p>Created At: {new Date(article.created_at).toUTCString()}</p>
+          <p>
+            Votes: {article.votes}
+            <button className="votes" onClick={increaseVoteByArticleId}>
+              👍+1{" "}
+            </button>
+          </p>
         </article>
       </main>
     </>
--- nc-news/api.js
+++ nc-news/api.js
@@ -21,3 +21,20 @@ export function fetchCommentsByArticleId(articleId) {
     return res.json();
   });
 }
+
+export function PatchArticlesByArticleId(articleId, votes) {
+  return fetch(
+    `https://seeding-nc-news-097y.onrender.com/api/articles/${articleId}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        inc_votes: votes,
+      }),
+    }
+  ).then((res) => {
+    return res.json();
+  });
+}
