d1095c5bd3b0b7771587aabd4f716a04a9db6443
Faria Hasan Talukder <faria1136@gmail.com>
2025-11-14 16:39:33 +0000

    Logo path updated

--- nc-news/Components/Logo.jsx
+++ nc-news/Components/Logo.jsx
@@ -1,5 +1,3 @@
 export default function Logo() {
-  return (
-    <img className="logo" src="../src/assets/logo.jpeg" alt="nc-logo"></img>
-  );
+  return <img className="logo" src="/logo.jpeg" alt="nc-logo"></img>;
 }
